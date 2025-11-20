e9711af9f2d14acf75088d12c509f1685c13125a
evjh <eva-calenberg@web.de>
2025-11-20 18:52:14 +0100

    Update inverse_kinematics.py

--- kinematics/inverse_kinematics.py
+++ kinematics/inverse_kinematics.py
@@ -23,14 +23,54 @@ class InverseKinematicsAgent(ForwardKinematicsAgent):
         :return: list of joint angles
         '''
         joint_angles = []
-        # YOUR CODE HERE
+        if effector_name not in ['LLeg', 'RLeg']:
+            return joint_angles
+
+        is_left_leg = (effector_name == 'LLeg')
+
+        x,y,z = transform[0,3], transform[1,3], transform[2,3]
+
+        sign = 1.0 if is_left_leg else -1.0
+        L_upper= 0.100
+        L_lower = 0.103
+
+        distance = (x**2+ y**2 + z**2) **05
+        try:  
+        hip_pitch = -atan2(x,-z)
+        cos_knee = (L_upper**2 + L_lower**2 - distance**2)/ (2*L_upper * L_lower)
+        knee_pitch = acos(max(min(cos_knee ,1),-1)
+        ankle_pitch = -hip_pitch -knee_pitch
+
+        hip_roll = atan2(y, -z)
+        ankle_roll = -hip_roll
+
+        if effector_name == 'LLeg':
+            joint_angles = [0.0, hip_roll, hip_pitch, knee_pitch, ankle_pitch, ankle_roll]
+        else: 
+            joint_angles = [0.0, hip_roll, hip_pitch, knee_pitch, ankle_pitch, ankle_roll]
+
+        except: joint_angles = [0.0, 0.0, -0.1, 0.7, -0.5, 0.0]
+        
         return joint_angles
 
     def set_transforms(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
-        # YOUR CODE HERE
-        self.keyframes = ([], [], [])  # the result joint angles have to fill in
+        joint_angles = self.inverse_kinematics(effector_name, transform)
+
+        if effector_name == 'LLeg':
+            joint_names = ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'LAnklePitch', 'LAnkleRoll']
+        else:
+            joint_names = ['RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch', 'RAnklePitch', 'RAnkleRoll']       
+
+        times = []
+        keys = []
+
+        for angle in joint_angles:
+            times.append([2.0])
+            keys.append([[angle, [3, 0, 0], [3,0 ,0]]])
+            
+        self.keyframes = (joint_names, times, keys)  # the result joint angles have to fill in
 
 if __name__ == '__main__':
     agent = InverseKinematicsAgent()
